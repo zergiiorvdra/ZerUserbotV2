8d819e2fd564cd85e8917e4238fa3ebe8a1a3d6a
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2023-11-21 10:13:01 +0700

    Update alive.py

--- ProjectMan/modules/alive.py
+++ ProjectMan/modules/alive.py
@@ -33,7 +33,7 @@ from .help import add_command_help
 
 modules = CMD_HELP
 alive_logo = (
-    gvarstatus("ALIVE_LOGO") or "https://telegra.ph/file/9dc4e335feaaf6a214818.jpg"
+    gvarstatus("ALIVE_LOGO") or "https://telegra.ph//file/faf09fc796d490199b492.jpg"
 )
 emoji = gvarstatus("ALIVE_EMOJI") or "⚡️"
 alive_text = gvarstatus("ALIVE_TEKS_CUSTOM") or "Hey, I am alive."
@@ -46,7 +46,7 @@ async def alive(client: Client, message: Message):
     send = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
     uptime = await get_readable_time((time.time() - StartTime))
     man = (
-        f"**[PyroMan-Userbot](https://github.com/mrismanaziz/PyroMan-Userbot) is Up and Running.**\n\n"
+        f"**[ZerPyro-Userbot](https://github.com/zergiiorvdra/ZerUserbotV2) is Up and Running.**\n\n"
         f"<b>{alive_text}</b>\n\n"
         f"{emoji} <b>Master :</b> {client.me.mention} \n"
         f"{emoji} <b>Modules :</b> <code>{len(modules)} Modules</code> \n"
