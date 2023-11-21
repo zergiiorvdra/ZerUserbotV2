e6a56f5d4973e745d61d8b90a168eda4084904e0
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2023-11-21 11:13:00 +0700

    Update misc.py

--- ProjectMan/utils/misc.py
+++ ProjectMan/utils/misc.py
@@ -18,7 +18,7 @@ admins_in_chat = {}
 
 
 def restart():
-    os.execvp(sys.executable, [sys.executable, "-m", "ProjectMan"])
+    os.execvp(sys.executable, [sys.executable, "-m", "ZerProject"])
 
 
 async def list_admins(client: Client, chat_id: int):
