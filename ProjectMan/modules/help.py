2792aa6466111f5aeeb7ee3cdf5f0ccdb2cde14a
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2023-11-21 11:05:02 +0700

    Update help.py

--- ProjectMan/modules/help.py
+++ ProjectMan/modules/help.py
@@ -28,12 +28,12 @@ async def module_help(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroMan-UserBot Modules"
+        ac.title = "ZerPyro-UserBot Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Lunatic0de × @SharingUserbot •"
+            message, f"```{str(ac)}```\n• @papladiesandboy × @cari_teman_virtual_ind •"
         )
         await message.reply(
             f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
@@ -45,7 +45,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Lunatic0de"
+            this_command += "© @cari_teman_virtual_ind"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
