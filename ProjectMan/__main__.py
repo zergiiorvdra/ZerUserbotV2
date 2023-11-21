88b58617104746e6cacef09be9ff7c30aabd7045
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2023-11-21 13:15:26 +0700

    Update __main__.py

--- ProjectMan/__main__.py
+++ ProjectMan/__main__.py
@@ -46,7 +46,7 @@ async def main():
             )
         except Exception as a:
             LOGGER("main").warning(a)
-    LOGGER("ProjectMan").info(f"PyroMan-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOGGER("ProjectZer").info(f"PyroZer-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
     if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
         await create_botlog(bot1)
     await idle()
@@ -54,7 +54,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    LOGGER("ProjectMan").info("Starting PyroMan-UserBot")
+    LOGGER("ProjectZer").info("Starting PyroZer-UserBot")
     install()
     heroku()
     LOOP.run_until_complete(main())
